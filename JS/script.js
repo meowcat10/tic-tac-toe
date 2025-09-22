97f5674a62e62b8fb302c7c5c4e847c72858471b
meowcat10 <Andreas@mitkas.com>
2025-09-22 11:27:19 +0200

    Fixed game mechanics and score

--- JS/script.js
+++ JS/script.js
@@ -2,19 +2,39 @@ const cells = document.querySelectorAll('.cell');
 const buttons = document.querySelectorAll('button');
 
 
-// const gameText = document.querySelector('.gameAction');
+const gameText = document.querySelector('.gameResult');
 
 const gameTurn = document.getElementById('playerTurn');
 const cursor = document.querySelector('#gameCursor');
-let cursorStatus = "ON";
+let cursorStatus = "OFF";
 
-gameField.forEach(field => {
-    field.addEventListener('click', () => {
-        if (gameTurn.value === 'X' && field.innerHTML === ''){
-            field.innerHTML = gameTurn.value;
+let X_wins = 0;
+let O_wins = 0;
+
+const winCombinations = [
+    [0, 1, 2], // Line ~ 1
+    [3, 4, 5], // Line ~ 2
+    [6, 7, 8], // Line ~ 3
+
+    [0, 3, 6], // Column ~ 1
+    [1, 4, 7], // Column ~ 2
+    [2, 5, 8], // Column ~ 3
+
+    [0, 4, 8], // Diagonal ~ 1
+    [2, 4, 6], // Diagonal ~ 2
+];
+
+
+setInterval(() => {
+    gameText.innerHTML = `Score<br>X: ${X_wins} O: ${O_wins}`;
+}, 2500);
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if (gameTurn.value === 'X' && cell.innerHTML === ''){
+            cell.innerHTML = gameTurn.value;
             gameTurn.value = 'O'
-        } else if (gameTurn.value === 'O' && field.innerHTML === ''){
-            field.innerHTML = gameTurn.value;
+        } else if (gameTurn.value === 'O' && cell.innerHTML === ''){
+            cell.innerHTML = gameTurn.value;
             gameTurn.value = 'X';
         }
         checkGame();
@@ -28,11 +48,8 @@ gameField.forEach(field => {
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
         /* RESET */
-        if (btn.innerHTML === 'Reset' || btn.innerHTML === 'New Game'){
-            cells.forEach(cell => {
-                cell.innerHTML = '';
-            })
-            btn.innerHTML = 'Reset';
+        if (btn.innerHTML === 'Reset'){
+            clearCells();
         }
 
         /* CURSOR */
@@ -50,23 +67,34 @@ buttons.forEach(btn => {
 
 
 
-/* If all are filled - New Game*/
+
 function checkGame() {
-    let checkCount = 0;
-    cells.forEach(cell => {
+    for (let combo of winCombinations) {
+        let [a, b, c] = combo;
+        let valA = cells[a].innerHTML;
+        let valB = cells[b].innerHTML;
+        let valC = cells[c].innerHTML;
+
+        if (valA === valB && valA === valC && valA !== ''){
+            gameText.innerHTML += `<br>The winner is ${valA}!`;
+            if (valA === 'X'){ X_wins++ }
+            if (valA === 'O'){ O_wins++ }
+            setTimeout(clearCells, 2000);
+            return valA;
+        }
+    }
+    let checkCells = 0;
+    cells.forEach((cell) => {
         if (cell.innerHTML !== ''){
-            checkCount++;
-            if (checkCount === 9){
-                buttons.forEach(btn => {
-                    if (btn.innerHTML === 'Reset'){
-                        btn.innerHTML = 'New Game';
-                    }
-                })
-            }
+            checkCells++;
+        }
+        if (checkCells === 9){
+            gameText.innerHTML += `<br>Draw!`;
+            setTimeout(clearCells, 2000);
         }
     })
+    return null;
 }
-
 
 
 
@@ -77,11 +105,18 @@ function checkGame() {
             cursor.style.display = "inline-block";
             cursor.innerHTML = gameTurn.value;
             cursor.animate({
-                left: e.clientX + "px",
-                top: e.clientY + "px",
+                left: e.clientX + 15 + "px",
+                top: e.clientY + 15 + "px",
             },{duration: 500, fill: "forwards"});
         } else {
             cursor.style.display = "none";
         }
     })
 
+
+
+function clearCells(){
+        cells.forEach(cell => {
+            cell.innerHTML = '';
+        })
+}
